329c09d66e7a700bf853407cf342a30a6fa3779f
Dekel <dekelb@gmail.com>
2020-11-02 20:45:05 +0200

    example docs

---
+++ example/README.md
@@ -0,0 +1,50 @@
+# Spring boot integration
+
+This example shows 2 methods for integrating with a PDP on a Spring boot application.
+
+1. Using a Web Security filter - this will simply indicate whether access is granted or not
+2. Using a Component which acts as a client - this method allows more flexibility as it 
+also allows examining the response from the PDP
+
+
+## Build the example
+
+In order to execute the example you should have the basic prerequisites for Java development
+ installed (JDK, Maven) and in addition, you will need to install the Spring PDP Client Jar.
+ (see https://github.com/build-security/opa-java-spring-client )
+
+Install the PDPClient JAR:
+
+    mvn install:install-file \
+    -Dfile=./opa-java-spring-client-0.1.0.jar \
+    -DgroupId=build.security \
+    -DartifactId=opa-java-spring-client \
+    -Dversion=0.1.0 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
+
+Verify that the project can be built (mvn install).
+
+Run the Spring Boot application.
+
+## Running and configuring the PDP
+
+You can manually configure the PDP with policy and data by following the instructions found
+ here:  https://github.com/build-security/opa-java-spring-client/#try-it-out
+ 
+You can also configure the policy and data using build.security's control-plane and 
+launching a PDP that was defined in the control plane.
+
+## Working with the example
+
+### Web Security
+
+Call the /websecurity endpoint in order to see the web-security filter working with the PDP
+
+    curl --location --request GET 'localhost:8080/webhook' 
+
+### PDP Client (SDK)
+
+Call the /sdk endpoint in order to have the PDP client interact with the PDP
+
+    curl --location --request GET 'localhost:8080/sdk'   
---
+++ example/src/main/java/security/build/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package security.build;
+
+import org.openpolicyagent.voter.OPAVoter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${pdp.port:9000}")
+    private String pdpPort;
+
+    @Value("${pdp.hostname}")
+    private String pdpHost;
+
+    @Value("${pdp.schema:http}")
+    private String pdpSchema;
+
+    @Value("${pdp.policy.path}")
+    private String pdpPolicyPath;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //configure the web security filter to go through the OPAVoter if accessing the /websecurity endpoint
+        http.authorizeRequests().antMatchers("/websecurity").authenticated().accessDecisionManager(accessDecisionManager());
+    }
+
+    public String getPdpPort() {
+        return pdpPort;
+    }
+
+    public String getPdpHost() {
+        return pdpHost;
+    }
+
+    public String getPdpPolicyPath() {
+        return pdpPolicyPath;
+    }
+
+    public String getPdpSchema() {
+        return pdpSchema;
+    }
+
+    @Bean
+    public AccessDecisionManager accessDecisionManager() {
+        String url = getPdpSchema() + "://" + getPdpHost() + ":" + getPdpPort() + "/v1/data" + getPdpPolicyPath();
+
+        List<AccessDecisionVoter<? extends Object>> decisionVoters = Arrays
+                .asList(new OPAVoter(url));
+        return new UnanimousBased(decisionVoters);
+    }
+
+}
