ac513bd24828f1030cf4418fbe75be5a1e8bbeeb
Amir Ben Nun <amir@build.security>
2021-01-17 18:09:45 +0200

    change interceptor

--- src/main/java/build/security/pdp/client/PdpInterceptor.java
+++ src/main/java/build/security/pdp/client/PdpInterceptor.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -20,21 +19,9 @@ public class PdpInterceptor extends HandlerInterceptorAdapter  {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws IOException {
-        System.out.println("amiramir string: " + handler.toString());
-        System.out.println("amiramir class: " + handler.getClass());
         HandlerMethod method = (HandlerMethod) handler;
-        AuthorizeAnnotation annotation = method.getMethodAnnotation(AuthorizeAnnotation.class);
-        Boolean present = method.getBeanType().isAnnotationPresent(AuthorizeAnnotation.class);
-        Boolean present2 = method.getBeanType().isAnnotationPresent(RequestMapping.class);
-        System.out.println("amiramir present: " + present);
-        System.out.println("amiramir present2: " + present2);
-        RequestMapping annotation2 = method.getMethodAnnotation(RequestMapping.class);
 
-        System.out.println("amiramir annotation: " + annotation);
-        System.out.println("amiramir annotation2: " + annotation2);
-
-        String[] resources = annotation == null ? new String[0] : annotation.resources();
-
+        String[] resources = new String[0];
         Boolean result = pdpEnforcer.RunAuthorization(request, resources);
         if (result) {
             return true;
