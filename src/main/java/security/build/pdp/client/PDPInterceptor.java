8a4d9e1f485c831a86c143b397f26e7179f95346
Amir Ben Nun <amir@build.security>
2021-01-19 11:18:07 +0200

    remove comments

--- src/main/java/security/build/pdp/client/PDPInterceptor.java
+++ src/main/java/security/build/pdp/client/PDPInterceptor.java
@@ -20,16 +20,11 @@ public class PDPInterceptor extends HandlerInterceptorAdapter  {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws IOException {
         HandlerMethod method = (HandlerMethod) handler;
-        System.out.println("amiramir method: " + method.toString());
         Class<Authorize> authorizeClass = Authorize.class;
-        System.out.println("amiramir annotation class: " + authorizeClass.toString());
         Authorize annotation = method.getMethodAnnotation(authorizeClass);
-        System.out.println("amiramir annotation instance: " + annotation.toString());
         String[] requirements = annotation != null ? annotation.resources() : new String[0];
-        System.out.println("amiramir annotation requirements: " + requirements.length);
 
         Boolean result = pdpEnforcer.AuthorizeRequest(request, requirements);
-        System.out.println("amiramir authz result: " + result);
         if (result) {
             return true;
         }
