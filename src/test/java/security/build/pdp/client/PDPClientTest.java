e960eb35de7f00c7cb46c13dbf7e5324eb6ab730
Amir Ben Nun <amir@build.security>
2021-01-19 12:35:03 +0200

    request provider test

--- src/test/java/security/build/pdp/client/PDPClientTest.java
+++ src/test/java/security/build/pdp/client/PDPClientTest.java
@@ -12,7 +12,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import security.build.pdp.client.PDPClient;
 import security.build.pdp.request.PDPRequest;
 
 import java.util.Map;
---
+++ src/test/java/security/build/pdp/request/PDPRequestProviderTest.java
@@ -0,0 +1,80 @@
+package security.build.pdp.request;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class PDPRequestProviderTest {
+    private static PDPRequestProvider PdpRequestProvider;
+
+    @BeforeAll
+    public static void setup() {
+        PdpRequestProvider = new PDPRequestProvider();
+    }
+
+    @Test
+    void Provide_ValidRequest_ContainsHeader() {
+        String headerName = "header-name";
+        String headerValue = "header-value";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader(headerName, headerValue);
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, new String[0]);
+        Assertions.assertEquals(headerValue, pdpRequest.input.request.headers.get(headerName));
+    }
+
+    @Test
+    void Provide_ForwardedRequest_ContainsOriginalSource() {
+        String sourceIp = "some-ip";
+        String wrongIp = "other-ip";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRemoteAddr(wrongIp);
+        request.addHeader("X-Forwarded-For", sourceIp);
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, new String[0]);
+        Assertions.assertEquals(sourceIp, pdpRequest.input.source);
+    }
+
+    @Test
+    void Provide_ValidRequest_ContainsSource() {
+        String sourceIp = "some-ip";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRemoteAddr(sourceIp);
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, new String[0]);
+        Assertions.assertEquals(sourceIp, pdpRequest.input.source);
+    }
+
+    @Test
+    void Provide_ValidRequest_ContainsDestination() {
+        String serverIp = "some-ip";
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setLocalAddr(serverIp);
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, new String[0]);
+        Assertions.assertEquals(serverIp, pdpRequest.input.destination);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"GET", "POST"})
+    void Provide_ValidRequest_ContainsHttpMethod(String method) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod(method);
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, new String[0]);
+        Assertions.assertEquals(method, pdpRequest.input.request.method);
+    }
+
+    @Test
+    void Provide_ValidRequest_ContainsRequirements() {
+        String[] resources = {"Hello", "World"};
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        PDPRequest pdpRequest = PdpRequestProvider.Provide(request, resources);
+        Assertions.assertArrayEquals(resources, pdpRequest.input.resources.requirements);
+    }
+}
